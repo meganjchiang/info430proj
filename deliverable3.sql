5e23db706c0326258d8f572344a749cd352e97be
meganjchiang <mjchiang@uw.edu>
2024-04-22 23:23:18 -0700

    add file for deliverable 3

---
+++ deliverable3.sql
@@ -0,0 +1,13 @@
+/* 
+INFO 430: Database Design and Management 
+Project Deliverable 3: Physical Design and Database Implementation
+Project Topic: Spotify Database
+Students: Evonne La & Megan Chiang
+Due Date: April 26, 2024
+*/
+
+-- Creating the Database and Table Structure
+
+-- Populating the Tables with Data
+
+-- Coding Database Objects
